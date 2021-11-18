52f76ca9222ef426b02a00f08b885dc8fbdc95e7
haeramkeem <ewqdsacxz2345@gmail.com>
2021-11-18 23:04:31 +0900

    feat: date changed event

--- ui/src/components/MiniCalendar.vue
+++ ui/src/components/MiniCalendar.vue
@@ -1,24 +1,33 @@
+// Show mini calendar below the header
+//      props:
+//          minify: Boolean
+//              Size of mini calendar
+//      emit:
+//          event name: dateChanged
+//          parameter:
+//              Date: changed date object
+
 <template>
     <div class="container-auto no-pad" id="mini-calendar">
         <div class="row align-items-center justify-content-center">
             <div v-if="this.minify" class="col-auto open-sans font-bold" id="mini-scale">{{strWeekDay}}, {{strFullDate}}</div>
             <div v-else class="col-auto no-pad" id="full-scale">
-                <button class="arrow-btn" id="yesterday-btn"></button>
+                <button class="arrow-btn" id="yesterday-btn" @click="gotoYesterday"></button>
                 <div class="text-center" id="day-date">
                     <div class="open-sans font-reg" id="week-day">{{strWeekDay}}</div>
-                    <div class="fira-sans font-bold" id="full-date">{{strFullDate}}</div>
+                    <div class="fira-sans font-bold" id="full-date">{{strDate}}</div>
                 </div>
-                <button class="arrow-btn" id="tomorrow-btn"></button>
+                <button class="arrow-btn" id="tomorrow-btn" @click="gotoTommorow" v-if="!isToday"></button>
+                <div v-else class="arrow-btn"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
 const date = new Date();
-
 export default defineComponent({
     name: "MiniCalendar",
     props: {
@@ -26,11 +35,38 @@ export default defineComponent({
     },
     data() {
         return {
-            year: date.getFullYear() - 2000,
-            month: date.getMonth() + 1,
-            day: date.getDate(),
+            strDate: "",
             weekDay: date.getDay(),
         };
+    },
+    methods: {
+        emitDateChanged() {
+            this.$emit("dateChanged", date);
+        },
+        updateCal() {
+            this.strDate = this.dtos(date);
+            this.weekDay = date.getDay();
+            this.emitDateChanged();
+        },
+        gotoYesterday() {
+            date.setDate(date.getDate() - 1);
+            this.updateCal();
+        },
+        gotoTommorow() {
+            date.setDate(date.getDate() + 1);
+            this.updateCal();
+        },
+        itos(digit: number) {
+            if(digit < 10) {
+                return "0" + digit;
+            }
+            return "" + digit;
+        },
+        dtos(target: Date) {
+            return this.itos(target.getFullYear() - 2000) +
+                this.itos(target.getMonth() + 1) +
+                this.itos(target.getDate());
+        },
     },
     computed: {
         strWeekDay() {
@@ -45,10 +81,15 @@ export default defineComponent({
             ];
             return week[this.weekDay];
         },
-        strFullDate() {
-            return `${this.year}${this.month}${this.day}`;
-        }
-    }
+        isToday() {
+            const today = new Date();
+            return this.strDate === this.dtos(today);
+        },
+    },
+    created() {
+        this.strDate = this.dtos(date);
+        this.emitDateChanged();
+    },
 });
 </script>
 
