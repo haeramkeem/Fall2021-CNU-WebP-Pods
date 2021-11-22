7b10a16efac623f32b1f52945c628d1e507bf7f3
haeramkeem <ewqdsacxz2345@gmail.com>
2021-11-22 20:59:18 +0900

    feat: Use docker image to CDP

--- run.sh
+++ run.sh
@@ -3,9 +3,31 @@
 cmd=$1
 
 case "$cmd" in
+
+    # UI related
     --ui-dev)
         cd ./ui && npm run serve
         ;;
+
+    # Chrome Dev Protocol related
+    --cdp-ins)
+        docker pull --platform linux/amd64 cdp chromedp/headless-shell:latest
+        ;;
+
+    --cdp-run)
+        docker run -d -p 9222:9222 --rm --name cdp --platform linux/amd64 -v $(pwd):/usr/local/pods chromedp/headless-shell
+        ;;
+
+    --cdp-sh)
+        docker exec -it cdp /bin/bash
+        ;;
+
+    # Build related
+    --build-amd64)
+        env GOOS=linux GOARCH=amd64 go build -o pods_amd64
+        ;;
+
+    # Unexpected
     *)
         echo "Unknown command '$cmd'"
         exit 1
