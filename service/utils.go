7d97f79339944a09053a4986893c9480223a2740
haeramkeem <ewqdsacxz2345@gmail.com>
2021-12-01 23:23:04 +0900

    feat: pods-dateformat to time.Time

---
+++ service/utils.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"crypto/rand"
+	"math/big"
+	"strconv"
+	"time"
+)
+
+const BASE string = `https://leetcode.com`;
+
+// Generate Random number
+func getRand(max int) (int, error) {
+    rand, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+    return int(rand.Int64()), err
+}
+
+var monthOf = map[string]time.Month{
+    "01":   time.January,
+    "02":   time.February,
+    "03":   time.March,
+    "04":   time.April,
+    "05":   time.May,
+    "06":   time.June,
+    "07":   time.July,
+    "08":   time.August,
+    "09":   time.September,
+    "10":   time.October,
+    "11":   time.November,
+    "12":   time.December,
+}
+
+func ParseStrDate(strDate string) time.Time {
+    strYear := strDate[:2]
+    strMonth := strDate[2:4]
+    strDay := strDate[4:]
+
+    nYear , _ := strconv.Atoi(strYear)
+    nYear += 2000
+    tMonth := monthOf[strMonth]
+    nDay, _ := strconv.Atoi(strDay)
+
+    loc, _ := time.LoadLocation("Asia/Seoul")
+
+    return time.Date(nYear, tMonth, nDay, 0, 0, 0, 0, loc)
+}
