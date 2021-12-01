895394292847855bcedf82e42c9754a452cf052d
haeramkeem <ewqdsacxz2345@gmail.com>
2021-12-01 20:11:42 +0900

    refactor: remove CDP docker image

--- run.sh
+++ run.sh
@@ -4,25 +4,12 @@ cmd=$1
 
 case "$cmd" in
 
-    # UI related
+    # Development
     --ui-dev)
         cd ./ui && npm run serve
         ;;
 
-    # Chrome Dev Protocol related
-    --cdp-ins)
-        docker pull --platform linux/amd64 cdp chromedp/headless-shell:latest
-        ;;
-
-    --cdp-run)
-        docker run -d -p 9222:9222 --rm --name cdp --platform linux/amd64 -v $(pwd):/usr/local/pods chromedp/headless-shell
-        ;;
-
-    --cdp-sh)
-        docker exec -it cdp /bin/bash
-        ;;
-
-    # Build related
+    # Build
     --build-amd64)
         env GOOS=linux GOARCH=amd64 go build -o pods_amd64
         ;;
