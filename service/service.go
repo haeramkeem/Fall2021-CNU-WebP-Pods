5daf7fac94510d2d3e9026186ac42d0e40275f16
haeramkeem <ewqdsacxz2345@gmail.com>
2021-12-02 16:36:18 +0900

    feat: main problem api

--- service/service.go
+++ service/service.go
@@ -1,14 +1,37 @@
 package service
 
 import (
+    "pods/modules/errors"
     . "pods/domain"
+    repo "pods/repository"
 )
 
 func GetMainProb(date string) (*ProblemJSON, error) {
+
+    // Get Problem from DB
+    prob, err := repo.SelectProblem(date, MAIN)
+    if err := errors.Check(err); err != nil {
+        return nil, err
+    }
+
+    if prob == nil {
+        // Fetch Problem
+        prob, err = FetchMain(date)
+        if err := errors.Check(err); err != nil {
+            return nil, err
+        }
+
+        // Save to DB
+        err = repo.CreateProblem(prob)
+        if err := errors.Check(err); err != nil {
+            return nil, err
+        }
+    }
+
     return &ProblemJSON{
-        Title: "testing title",
-        Link: "testing link",
-        Description: "testing description",
+        Title: prob.Title,
+        Link: prob.Link,
+        Description: prob.Description,
     }, nil
 }
 
